53375ee8ccbc1db7ddf1565411dff0a27d7fb3e3
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-05-04 10:43:54 +0700

    insert one and many data

---
+++ README.md
@@ -0,0 +1,7 @@
+** MONGODB **
+
+DAY 1
+
+\_ CRUD Collection
+
+\_ Insert one and many data
--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -2,11 +2,46 @@
 use('mongodb_c2409');
 
 // tạo collection
-db.createCollection('category');
-db.createCollection('student');
+// db.createCollection('category');
+// db.createCollection('student');
 
 // xoá collection
-db.category.drop();
+// db.category.drop();
 
 // đổi tên collection
-db.student.renameCollection('student_new');
+// db.new_student.renameCollection('newStudent');
+
+// insert data
+// db.newStudent.insertOne({
+//   name: 'Dave',
+//   age: 20,
+//   address: 'Saigon',
+// });
+
+// db.newStudent.insertOne({
+//   name: 'Diddy',
+//   age: 20,
+//   address: 'Saigon',
+//   gmail: 'diddy@gmail.com',
+// });
+
+db.newStudent.insertMany([
+  {
+    name: 'D',
+    age: 20,
+    address: 'Saigon',
+    gmail: 'd@gmail.com',
+  },
+  {
+    name: 'E',
+    age: 20,
+    address: 'Saigon',
+    gmail: 'e@gmail.com',
+  },
+  {
+    name: 'F',
+    age: 20,
+    address: 'Saigon',
+    gmail: 'f@gmail.com',
+  },
+]);
