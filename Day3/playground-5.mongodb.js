c2a232383b73f7435a338bb5028bcd44355241a3
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-05-08 09:54:28 +0700

    skip, limit keyword in mongodb

---
+++ Day3/playground-5.mongodb.js
@@ -0,0 +1,35 @@
+use('mongodb_c2409');
+
+// tăng dần
+db.product.find().sort({
+  price: 1,
+});
+
+// giảm dần kèm status true
+db.product
+  .find({
+    status: true,
+  })
+  .sort({
+    price: -1,
+  });
+
+// chỉ lấy 2 sản phẩm theo giá giảm dần, đặc biệt bỏ sản phẩm đầu tiên
+db.find()
+  .sort({
+    price: -1,
+  })
+  .skip(1)
+  .limit(2);
+
+// select * from product
+// select id, name, price from product
+// 1 hiển thị 0 ẩn
+db.product.find(
+  {},
+  {
+    name: 1,
+    price: 1,
+    _id: 0,
+  },
+);
